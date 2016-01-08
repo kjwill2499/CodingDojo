f66f81849d1dc986a73520efc42c7fe6b9ffbe95
Kenrick Williams <kenrickj84@gmail.comn>
2016-01-08 12:58:51 -0800

    adds the js_scroll assignment

--- LAMP/AJAX/notes/application/models/note.php
+++ LAMP/AJAX/notes/application/models/note.php
@@ -1,26 +1,29 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Post extends CI_Model {
+class Note extends CI_Model {
 
 	public function get_all(){
-		$query = 'SELECT * FROM posts';
+		$query = 'SELECT * FROM notes';
 		return $this->db->query($query)->result_array();
 	}
-	public function show($post_id){
-		$query = "SELECT * FROM posts WHERE id = ?";
-		return $this->db->query($query, array($post_id))->row_array();
+	public function show($note_id){
+		$query = "SELECT * FROM notes WHERE id = ?";
+		return $this->db->query($query, array($note_id))->row_array();
 	}
-
-	public function create($newPost){
-		$query = "INSERT INTO posts (description, created_at, updated_at) VALUES (?,?,?)";
-		$values = array($newPost, date("Y-m-d, H:i:s"), date("Y-m-d, H:i:s"));
+	public function add_description($note_id){
+		$query = "INSERT INTO notes (description, created_at, updated_at) VALUES (?,?,?)";
+		$values = array($newNote, date("Y-m-d, H:i:s"), date("Y-m-d, H:i:s"));
+	}
+	public function create($newNote){
+		$query = "INSERT INTO notes (description, created_at, updated_at) VALUES (?,?,?)";
+		$values = array($newNote, date("Y-m-d, H:i:s"), date("Y-m-d, H:i:s"));
 		$id = $this->db->insert_id($this->db->query($query, $values));
 		return $id;
 	}
-	public function destroy($post_id){
-		$query = "DELETE FROM posts WHERE id = ?";
+	public function destroy($note_id){
+		$query = "DELETE FROM notes WHERE id = ?";
 		$this->db->query($query, array($user_id));
-		return $post_id;
+		return $note_id;
 	}
 }
 
